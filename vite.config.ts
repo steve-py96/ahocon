3d95f20fd056a65a38cab769fdc38d14b9b1832f
steve <steve.scheder@avenga.com>
2021-12-28 23:49:47 +0100

    improved export format

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { resolve } from 'node:path';
 import { cp } from 'node:fs/promises';
 
 export default defineConfig(async () => {
-  await cp(resolve(__dirname, 'src/_global.d.ts'), resolve(__dirname, 'dist/index.d.ts'));
+  await cp(resolve(__dirname, 'src/_global.d.ts'), resolve(__dirname, 'dist/_global.d.ts'));
 
   return {
     build: {
       lib: {
-        entry: '/src/main.ts',
+        entry: '/src/index.ts',
         name: 'stocon',
         fileName: (format) => `index.${format}.js`,
       },
